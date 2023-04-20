44bb7bbf5d87c2c76a30afa9148d6a6b257c9527
lamya-23 <lamyaa.ouardirhi@gmail.com>
2023-04-20 09:20:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main (void)
+/**
+* main - entry point
+* Return: 0 (succeSS)
+*/
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
